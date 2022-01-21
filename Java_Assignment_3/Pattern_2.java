37527d959446b78e1270b310505bd6bdea1cadeb
Subham Kedia <49448247+notsocommon15@users.noreply.github.com>
2022-01-21 15:22:25 +0530

    Create Pattern_2.java

---
+++ Java_Assignment_3/Pattern_2.java
@@ -0,0 +1,15 @@
+public class Pattern_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=5;i>0;i--)
+		{
+			for(int j=i;j>0;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
