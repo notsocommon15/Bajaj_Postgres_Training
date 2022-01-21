91c84cdfb3a2b0996a46f1d495218e509766ff6f
Subham Kedia <49448247+notsocommon15@users.noreply.github.com>
2022-01-21 12:48:23 +0530

    Add files via upload

---
+++ Java_Assignement_1/Sum10.java
@@ -0,0 +1,14 @@
+
+public class Sum10 {
+public static void main(String[] args)
+{
+	int i = 1;
+	int s = 0;
+	while(i<=10)
+	{
+		s+=i;
+		i++;
+	}
+	System.out.println("Sum of first 10 numbers = "+ s);
+}
+}
